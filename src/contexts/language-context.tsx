01373d48f293f3e43235b64b8726e957f6210d57
iosebi1999 <iosebi@hlwgroup.dev>
2025-03-05 11:53:07 +0400

    Deployinh

--- src/contexts/language-context.tsx
+++ src/contexts/language-context.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { useState, useEffect, useContext, createContext } from 'react';
 
 export enum Language {
   KA = 'KA',
