3b6a6634432717fb0dd1ca5659302ff28817ff6a
Josph1999 <soso.guruli@gmail.com>
2025-09-18 00:39:10 +0400

    Removed photo

--- src/components/september-event/september-event.tsx
+++ src/components/september-event/september-event.tsx
@@ -96,89 +96,89 @@ export function SeptemberEvent() {
       </Box>
       {tab === 'dayplan' ? (
         <Box
+          sx={{
+            backgroundImage: `url(${CONFIG.assetsDir}/assets/images/gegma-01.png)`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed',
+            backgroundPosition: 'center top',
+            display: 'flex',
+            padding: '5px',
+            justifyContent: 'flex-start',
+            flexDirection: 'column',
+            gap: '25px',
+            fontFamily: 'UpperCaseHelvetica1 !important',
+            fontFeatureSettings: "'case' on",
+            color: 'white',
+          }}
+        >
+          <Typography
             sx={{
-              backgroundImage: `url(${CONFIG.assetsDir}/assets/images/gegma-01.png)`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-              backgroundAttachment: 'fixed',
-              backgroundPosition: 'center top',
-              display: 'flex',
-              padding: '5px',
-              justifyContent: 'flex-start',
-              flexDirection: 'column',
-              gap: '25px',
               fontFamily: 'UpperCaseHelvetica1 !important',
               fontFeatureSettings: "'case' on",
-              color: 'white',
+              fontSize: '40px',
+              textAlign: 'center',
             }}
           >
-            <Typography
-              sx={{
-                fontFamily: 'UpperCaseHelvetica1 !important',
-                fontFeatureSettings: "'case' on",
-                fontSize: '40px',
-                textAlign: 'center',
-              }}
-            >
-              {renderLanguage('დღის გეგმა', 'Day plan')}
-            </Typography>
-            {texts.map((item) => (
-              <Box
-                key={item.id}
-                sx={{
-                  display: 'flex',
-                  gap: '12px',
-                  alignItems: 'center', // align text from the top
-                  justifyContent: 'flex-start',
-                }}
-              >
-                <Image
-                  src={`${CONFIG.assetsDir}/assets/images/gegma-03.png`}
-                  sx={{
-                    objectFit: 'contain',
-                    height: '70px',
-                    width: '70px',
-                    flexShrink: 0, // prevents shrinking on small screens
-                    mt: '4px', // push icon a bit down to align better
-                  }}
-                />
-                <Typography
-                  sx={{
-                    fontFamily: 'UpperCaseHelvetica1 !important',
-                    fontFeatureSettings: "'case' on",
-                    fontSize: '18px',
-                    lineHeight: 1.6, // consistent spacing
-                    wordBreak: 'break-word', // wrap long Georgian text gracefully
-                  }}
-                >
-                  {renderLanguage(item.ka, item.en)}
-                </Typography>
-              </Box>
-            ))}
+            {renderLanguage('დღის გეგმა', 'Day plan')}
+          </Typography>
+          {texts.map((item) => (
             <Box
+              key={item.id}
               sx={{
-                width: '100%',
                 display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                gap: '12px',
+                alignItems: 'center', // align text from the top
+                justifyContent: 'flex-start',
               }}
             >
               <Image
-                src={`${CONFIG.assetsDir}/assets/images/${language === Language.KA ? 'gegma-04.png' : 'gegma-05.png'}`}
+                src={`${CONFIG.assetsDir}/assets/images/gegma-03.png`}
                 sx={{
                   objectFit: 'contain',
-                  height: '120px',
-                  width: '120px',
+                  height: '70px',
+                  width: '70px',
+                  flexShrink: 0, // prevents shrinking on small screens
+                  mt: '4px', // push icon a bit down to align better
                 }}
               />
+              <Typography
+                sx={{
+                  fontFamily: 'UpperCaseHelvetica1 !important',
+                  fontFeatureSettings: "'case' on",
+                  fontSize: '18px',
+                  lineHeight: 1.6, // consistent spacing
+                  wordBreak: 'break-word', // wrap long Georgian text gracefully
+                }}
+              >
+                {renderLanguage(item.ka, item.en)}
+              </Typography>
             </Box>
+          ))}
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              src={`${CONFIG.assetsDir}/assets/images/${language === Language.KA ? 'gegma-04.png' : 'gegma-05.png'}`}
+              sx={{
+                objectFit: 'contain',
+                height: '120px',
+                width: '120px',
+              }}
+            />
           </Box>
+        </Box>
       ) : (
-        <div >
+        <div>
           <TransformWrapper initialScale={1} minScale={0.5} maxScale={5} centerOnInit>
             <TransformComponent>
               <Image
-                src={`${CONFIG.assetsDir}/assets/images/map.jpg`}
+                src="https://lead-for-test.s3.eu-north-1.amazonaws.com/%E1%83%92%E1%83%94%E1%83%92%E1%83%9B%E1%83%90_%E1%83%A1%E1%83%90%E1%83%91%E1%83%90%E1%83%93%E1%83%9D-01.jpg"
                 sx={{
                   width: 'auto',
                   objectFit: 'contain',
