21cc2ca0ee952590817329a9e17bc1a403c2041a
Ioseb Guruli <iosebguruli@admins-MacBook-Pro.local>
2026-01-29 23:06:50 +0400

    changed stuff

--- src/sections/about/data.ts
+++ src/sections/about/data.ts
@@ -27,4 +27,27 @@ David Kldiashvili - Campus Manager
 
 David holds a Bachelor's degree from Tbilisi State University (Javakhishvili) and a Master’s degree from Erasmus University Rotterdam and LUMSA University in Rome.`,
   },
+  {
+    id: 3,
+    name_ka: 'გიორგი ანდრონიკაშვილი',
+    name: 'Giorgi Andronikashvili',
+    position_ka: 'კამპუსის მენეჯერი',
+    position: 'Campus Manager',
+    avatarUrl: `${CONFIG.assetsDir}/assets/images/mock/portrait/portrait-1.webp`,
+    description_ka: `გიორგის აქვს ხანგრძილივი გამოცდილება არაფორმალური განათლების სფეროში.
+
+2021-24 წლებში გიორგი იყო „ფრანკლინის აკადემიის“ ლექტორი. იგი ხელმძღვანელობდა Franklin Alumni-ს, აკადემიის კურსდამთავრებულების გაერთიანებას.
+
+2023 წლიდან იგი არის High School of Georgias ხელოვნების პროგრამის ხელმძღვანელი და ლექტორი.
+
+2018 წლიდან, „ევროპულ-ქართულ ინსტიტუტთან“ ერთად გიორგი მონაწილეობს სხვადასხვა საგანმანათლებლო და ახალგაზრდებზე ორიენტირებული პროექტების შექმნაში.
+
+2018 წლიდან მუშაობს, როგორც ტრენერი და მსაჯი დებატების ხელოვნებაში, სხვადასხვა საგანმანათლებლო პროექტებში.`,
+    description_en: `Giorgi Andronikashvoli - Campus Manager 
+Giorgi has extensive experience in the field of non-formal education.
+From 2021 to 2024, Giorgi was a lecturer at Franklin Academy. He also led Franklin Alumni, the academy’s graduates’ association.
+Since 2023, he has been the head of the Arts Program and a lecturer at the High School of Georgia.
+Since 2018, in cooperation with the European-Georgian Institute, Giorgi has been involved in the creation of various educational and youth-oriented projects.
+Since 2018, he has also worked as a trainer and judge in the art of debate in various educational projects.`,
+  },
 ];
--- src/sections/about/team.tsx
+++ src/sections/about/team.tsx
@@ -19,7 +19,7 @@ export function OurTeam() {
       xs: 1,
       sm: 2,
       md: 3,
-      lg: 4,
+      lg: 3,
     },
   });
 
